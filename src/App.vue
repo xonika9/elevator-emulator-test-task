5d11350e877ee0c4b16b9bb67dca1125a533e1b5
xonika9 <xonika9@gmail.com>
2023-04-11 18:16:19 +0300

    add multiple elevators

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,143 @@
 <script setup>
-import { ref, reactive, watch } from 'vue';
+import { ref, reactive, watch, onMounted } from 'vue';
 import ElevatorShaft from './components/ElevatorShaft/ElevatorShaft.vue';
 import ElevatorFloors from './components/ElevatorFloors/ElevatorFloors.vue';
 import ElevatorCab from './components/ElevatorCab/ElevatorCab.vue';
-import { NUM_FLOORS } from './config';
+import { NUM_FLOORS, NUM_ELEVATORS } from './config';
 
-const currentFloor = ref(1);
-const targetFloor = reactive({ value: 1 });
-const direction = ref('');
-const activeCalls = reactive(new Array(NUM_FLOORS).fill(false));
-const arrivedFloors = reactive(new Array(NUM_FLOORS).fill(false));
-const callQueue = ref([]);
-const movingToTarget = ref(false);
-const storedState = JSON.parse(localStorage.getItem('elevatorState') || '{}');
-
-currentFloor.value = storedState.currentFloor || 1;
-targetFloor.value = storedState.targetFloor || 1;
-callQueue.value = storedState.callQueue || [];
-
-const saveState = () => {
-  const state = {
-    currentFloor: currentFloor.value,
-    targetFloor: targetFloor.value,
-    callQueue: callQueue.value,
+const createElevator = () => {
+  const elevator = {
+    currentFloor: ref(1),
+    targetFloor: reactive({ value: 1 }),
+    direction: ref(''),
+    activeCalls: reactive(new Array(NUM_FLOORS).fill(false)),
+    arrivedFloors: reactive(new Array(NUM_FLOORS).fill(false)),
+    callQueue: ref([]),
+    movingToTarget: ref(false),
   };
-  localStorage.setItem('elevatorState', JSON.stringify(state));
+
+  watch(elevator.currentFloor, (newFloor, oldFloor) => {
+    elevator.direction.value = newFloor > oldFloor ? '↑' : '↓';
+  });
+
+  return elevator;
 };
 
-const callElevator = async (floor) => {
-  if (
-    (currentFloor.value === floor && !movingToTarget.value) ||
-    (targetFloor.value === floor && movingToTarget.value) ||
-    callQueue.value.includes(floor)
-  ) {
-    return;
-  }
+const saveState = (elevator, index) => {
+  const state = {
+    currentFloor: elevator.currentFloor.value,
+    targetFloor: elevator.targetFloor.value,
+    callQueue: elevator.callQueue.value,
+  };
+  localStorage.setItem(`elevatorState-${index}`, JSON.stringify(state));
+};
 
-  callQueue.value.push(floor);
-  activeCalls[floor - 1] = true;
-  saveState();
-  if (movingToTarget.value) return;
+const restoreState = (elevator, index) => {
+  const storedState = JSON.parse(
+    localStorage.getItem(`elevatorState-${index}`) || '{}'
+  );
 
-  while (callQueue.value.length > 0) {
-    movingToTarget.value = true;
-    targetFloor.value = callQueue.value[0];
-    activeCalls[targetFloor.value - 1] = true;
-    const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  elevator.currentFloor.value = storedState.currentFloor || 1;
+  elevator.targetFloor.value = storedState.targetFloor || 1;
+  elevator.callQueue.value = storedState.callQueue || [];
+};
 
-    while (currentFloor.value !== targetFloor.value) {
-      if (targetFloor.value > currentFloor.value) {
-        currentFloor.value++;
-      } else {
-        currentFloor.value--;
-      }
-      await delay(1000);
+const operateElevator = (elevator, index) => {
+  restoreState(elevator, index);
+
+  const callElevator = async (floor) => {
+    if (
+      (elevator.currentFloor.value === floor &&
+        !elevator.movingToTarget.value) ||
+      (elevator.targetFloor.value === floor && elevator.movingToTarget.value) ||
+      elevator.callQueue.value.includes(floor)
+    ) {
+      return;
     }
 
-    arrivedFloors[targetFloor.value - 1] = true;
-    await delay(3000);
-    activeCalls[targetFloor.value - 1] = false;
-    arrivedFloors[targetFloor.value - 1] = false;
-    callQueue.value.shift();
-    movingToTarget.value = false;
-    saveState();
-  }
+    elevator.callQueue.value.push(floor);
+    elevator.activeCalls[floor - 1] = true;
+    saveState(elevator, index);
+
+    if (elevator.movingToTarget.value) return;
+    while (elevator.callQueue.value.length > 0) {
+      elevator.movingToTarget.value = true;
+      elevator.targetFloor.value = elevator.callQueue.value[0];
+      elevator.activeCalls[elevator.targetFloor.value - 1] = true;
+      const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+      while (elevator.currentFloor.value !== elevator.targetFloor.value) {
+        if (elevator.targetFloor.value > elevator.currentFloor.value) {
+          elevator.currentFloor.value++;
+        } else {
+          elevator.currentFloor.value--;
+        }
+        await delay(1000);
+      }
+
+      elevator.arrivedFloors[elevator.targetFloor.value - 1] = true;
+      await delay(3000);
+      elevator.activeCalls[elevator.targetFloor.value - 1] = false;
+      elevator.arrivedFloors[elevator.targetFloor.value - 1] = false;
+      elevator.callQueue.value.shift();
+      elevator.movingToTarget.value = false;
+      saveState(elevator, index);
+    }
+  };
+
+  return callElevator;
 };
 
-watch(currentFloor, (newFloor, oldFloor) => {
-  direction.value = newFloor > oldFloor ? '↑' : '↓';
+const elevators = new Array(NUM_ELEVATORS).fill(null).map((_, index) => {
+  const elevator = createElevator();
+  const callElevator = operateElevator(elevator, index);
+  return { ...elevator, callElevator };
 });
+
+const callElevator = async (floor) => {
+  const availableElevator = elevators.find(
+    (elevator) => !elevator.movingToTarget.value
+  );
+  const elevatorToUse = availableElevator || elevators[0];
+  await elevatorToUse.callElevator(floor);
+};
 </script>
 
 <template>
-  <elevator-shaft />
+  <div class="elevator-shafts">
+    <elevator-shaft v-for="(elevator, index) in elevators" :key="index" />
+  </div>
   <elevator-floors
     :callElevator="callElevator"
-    :activeCalls="activeCalls"
-    :arrivedFloors="arrivedFloors"
+    :activeCalls="elevators[0].activeCalls"
+    :arrivedFloors="elevators[0].arrivedFloors"
     :numFloors="NUM_FLOORS"
   />
-  <elevator-cab
-    :currentFloor="currentFloor"
-    :direction="direction"
-    :targetFloor="targetFloor.value"
-    :activeCalls="activeCalls"
-    :numFloors="NUM_FLOORS"
-  />
+  <div class="elevator-cabs">
+    <elevator-cab
+      v-for="(elevator, index) in elevators"
+      :key="index"
+      :currentFloor="elevator.currentFloor.value"
+      :direction="elevator.direction.value"
+      :targetFloor="elevator.targetFloor.value"
+      :activeCalls="elevator.activeCalls"
+      :numFloors="NUM_FLOORS"
+    />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.elevator-shafts {
+  display: flex;
+  align-items: flex-end;
+}
+
+.elevator-cabs {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: flex-end;
+  height: 100%;
+}
+</style>
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const NUM_FLOORS = 5;
+export const NUM_ELEVATORS = 3;
